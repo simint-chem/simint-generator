81ced22974a3108b880132384464de025c067262
Ben Pritchard <ben@bennyp.org>
2015-05-19 17:32:01 -0400

    new testing program. Test all valid quartets

--- test/test_eri.cpp
+++ test/test_eri.cpp
@@ -7,7 +7,7 @@
 
 #define NCART(am) ((am>=0)?((((am)+2)*((am)+1))>>1):0)
 
-#define MAXAM 3
+#define MAXAM 2
 
 typedef int (*erifunc)(struct multishell_pair const, struct multishell_pair const, double * const restrict);
 
@@ -25,6 +25,18 @@ int eri_notyetimplemented(struct multishell_pair const P,
 }
 
 
+bool ValidQuartet(std::array<int, 4> am)
+{
+    if(am[0] < am[1])
+        return false;
+    if(am[2] < am[3])
+        return false;
+    if( (am[0] + am[1]) < (am[2] + am[3]) )
+        return false;
+    if(am[0] < am[2])
+        return false;
+    return true;
+}
 
 
 int main(int argc, char ** argv)
@@ -61,13 +73,10 @@ int main(int argc, char ** argv)
     funcs[2][2][2][2] = eri_FO_d_d_d_d;
 
 
-    funcs[3][0][0][0] = eri_FO_d_p_s_s;
-    funcs[2][0][0][0] = eri_FO_d_p_s_s;
 
-
-    if(argc != 13)
+    if(argc != 9)
     {
-        printf("Give me 12 arguments! I got %d\n", argc-1);
+        printf("Give me 8 arguments! I got %d\n", argc-1);
         return 1;
     }
 
@@ -75,69 +84,107 @@ int main(int argc, char ** argv)
 
     std::array<int, 4> nshell = {atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4])};
     std::array<int, 4> nprim = {atoi(argv[5]), atoi(argv[6]), atoi(argv[7]), atoi(argv[8])};
-    std::array<int, 4> am = {atoi(argv[9]), atoi(argv[10]), atoi(argv[11]), atoi(argv[12])};
+
+    const int maxncart = pow(NCART(MAXAM), 4);
+
+    const int nshell1234 = nshell[0] * nshell[1] * nshell[2] * nshell[3];
 
 
-    int nshell1234 = nshell[0] * nshell[1] * nshell[2] * nshell[3];
-
-    int ncart = NCART(am[0]) * NCART(am[1]) * NCART(am[2]) * NCART(am[3]);   
-
-    /* Storage of test results */
-    double * res_FOcombined      = (double *)ALLOC(64*ncart * nshell1234 * sizeof(double));
-    double * res_liberd          = (double *)ALLOC(64*ncart * nshell1234 * sizeof(double));
-    double * res_valeev          = (double *)ALLOC(64*ncart * nshell1234 * sizeof(double));
-
-    // allocate gaussian shell memory
-    VecQuartet gshells(  CreateRandomQuartets(nshell, nprim, am) );
-
+    // doesn't do anything at the moment
     Boys_Init(0, 7); // need F0 + 7 for interpolation
+
+    // initialize valeev stuff
     Valeev_Init();
 
-    // Actually calculate
-    struct multishell_pair P = create_multishell_pair(nshell[0], gshells[0].data(),
-                                                      nshell[1], gshells[1].data());
-    struct multishell_pair Q = create_multishell_pair(nshell[2], gshells[2].data(),
-                                                      nshell[3], gshells[3].data());
-
-    funcs[am[0]][am[1]][am[2]][am[3]](P, Q, res_FOcombined);
-
-    // test with valeev & liberd
-    ValeevIntegrals(gshells, res_valeev);
-    //ERDIntegrals(gshells, res_liberd);
 
 
-    printf("( %d %d | %d %d )\n", am[0], am[1], am[2], am[3]);
-    printf("%22s %22s %22s\n", "liberd", "FOcombined", "valeev");
+    /* Storage of test results */
+    double * res_FO              = (double *)ALLOC(maxncart * nshell1234 * sizeof(double));
+    //double * res_liberd          = (double *)ALLOC(maxncart * nshell1234 * sizeof(double));
+    double * res_valeev          = (double *)ALLOC(maxncart * nshell1234 * sizeof(double));
 
-    int idx = 0;
-    for(int i = 0; i < nshell1234; i++)
+
+    printf("\n");
+    printf("%16s  %20s  %20s\n", "Quartet", "MaxErr", "MaxRelErr");
+
+    // loop over all quartets, choosing only valid ones
+    for(int i = 0; i <= MAXAM; i++)
+    for(int j = 0; j <= MAXAM; j++)
+    for(int k = 0; k <= MAXAM; k++)
+    for(int l = 0; l <= MAXAM; l++)
     {
-        for(int j = 0; j < ncart; j++)
+        std::array<int, 4> am{i, j, k, l};
+
+        if(!ValidQuartet(am))
+            continue;
+ 
+        int ncart = NCART(i) * NCART(j) * NCART(k) * NCART(l);   
+
+
+        // allocate gaussian shell memory
+        VecQuartet gshells(  CreateRandomQuartets(nshell, nprim, am) );
+
+
+        // Actually calculate
+        struct multishell_pair P = create_multishell_pair(nshell[0], gshells[0].data(),
+                                                          nshell[1], gshells[1].data());
+        struct multishell_pair Q = create_multishell_pair(nshell[2], gshells[2].data(),
+                                                          nshell[3], gshells[3].data());
+
+
+        // calcualate with my code
+        funcs[am[0]][am[1]][am[2]][am[3]](P, Q, res_FO);
+
+        // test with valeev & liberd
+        ValeevIntegrals(gshells, res_valeev);
+        //ERDIntegrals(gshells, res_liberd);
+
+
+        //printf("( %d %d | %d %d )\n", am[0], am[1], am[2], am[3]);
+        //printf("%22s %22s %22s\n", "liberd", "FO", "valeev");
+
+        double maxerr = 0;
+        double maxrelerr = 0;
+
+        int idx = 0;
+        for(int i = 0; i < nshell1234; i++)
         {
-            printf("%22.4e  %22.4e  %22.4e\n", res_liberd[idx], res_FOcombined[idx], res_valeev[idx]);
+            for(int j = 0; j < ncart; j++)
+            {
+                //printf("%22.4e  %22.4e  %22.4e\n", res_liberd[idx], res_FO[idx], res_valeev[idx]);
 
-            const double v = res_valeev[idx];
-            double diff_liberd         = fabs(res_liberd[idx]         - v);
-            double diff_FOcombined     = fabs(res_FOcombined[idx]     - v);
-            printf("%22.4e  %22.4e\n", diff_liberd, diff_FOcombined);
-            printf("\n");
+                const double v = res_valeev[idx];
+                //double diff_liberd  = fabs(res_liberd[idx]         - v);
+                double diff_FO      = fabs(res_FO[idx]     - v);
+                double rel_FO       = fabs(diff_FO / v);
+                //printf("%22.4e  %22.4e\n", diff_liberd, diff_FO);
+                //printf("\n");
 
-            idx++;
+                if(diff_FO > maxerr)
+                    maxerr= diff_FO;
+                if(rel_FO > maxrelerr)
+                    maxrelerr = rel_FO;
+
+                idx++;
+            }
+            //printf("\n");
         }
-        printf("\n");
+
+        printf("( %2d %2d | %2d %2d )    %20.8e   %20.8e\n", am[0], am[1], am[2], am[3], maxerr, maxrelerr);
+
+        free_multishell_pair(P);
+        free_multishell_pair(Q);
+        FreeRandomQuartets(gshells);
     }
 
-    free_multishell_pair(P);
-    free_multishell_pair(Q);
-
-    FreeRandomQuartets(gshells);
+    printf("\n");
 
     Valeev_Finalize();
     Boys_Finalize();
 
     FREE(res_valeev);
-    FREE(res_liberd);
-    FREE(res_FOcombined);
+    //FREE(res_liberd);
+    FREE(res_FO);
 
     return 0;
 }
