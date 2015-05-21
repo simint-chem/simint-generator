b4fe0a2664625ecfe0d85b196d149f0b93370cbb
Ben Pritchard <ben@bennyp.org>
2015-05-21 17:11:15 -0400

    Modify gen script

--- gen.sh
+++ gen.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-python3 ./generate_twoel.py -l 2 \
-                            -b FO \
-                            -p FO \
-                            -g build/generator/generator \
-                            -d eri/FO
+python3 ../generate_twoel.py -l 2 \
+                             -b FO \
+                             -p FO \
+                             -g generator/generator \
+                             -d ../eri/FO
 
-python3 ./generate_twoel.py -l 2 \
-                            -b split \
-                            -p split \
-                            -g build/generator/generator \
-                            -d eri/split
+python3 ../generate_twoel.py -l 2 \
+                             -b split \
+                             -p split \
+                             -g generator/generator \
+                             -d ../eri/split
 
-python3 ./generate_twoel.py -l 3 \
-                            -b vref \
-                            -p vref \
-                            -g build/generator/generator \
-                            -d eri/vref
+python3 ../generate_twoel.py -l 3 \
+                             -b vref \
+                             -p vref \
+                             -g generator/generator \
+                             -d ../eri/vref
 
 touch eri/CMakeLists.txt
