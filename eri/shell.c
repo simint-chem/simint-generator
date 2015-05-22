f77298dfd08b2cb8c6417f20e9b2552a0158c372
Ben Pritchard <ben@bennyp.org>
2015-05-22 15:27:14 -0400

    Add flattened shell pair

--- eri/shell.c
+++ eri/shell.c
@@ -108,7 +108,52 @@ void allocate_multishell_pair(int na, struct gaussian_shell const * const restri
     P->nprim12 = intmem;
     P->primstart = intmem + na*nb;
     P->primend = intmem + 2*na*nb;
+}
 
+
+// Allocates a shell pair with correct alignment
+// Only fills in nprim_length member
+void allocate_multishell_pair_flat(int na, struct gaussian_shell const * const restrict A,
+                                   int nb, struct gaussian_shell const * const restrict B,
+                                   struct multishell_pair_flat * const restrict P)
+{
+    ASSUME_ALIGN(A);
+    ASSUME_ALIGN(B);
+
+    int prim_size = 0;
+
+    // with rounding up to the nearest boundary
+    for(int i = 0; i < na; ++i)
+    for(int j = 0; j < nb; ++j)
+        prim_size += SIMD_ROUND_DBL(A[i].nprim * B[j].nprim);
+
+    const int shell12_size = na*nb;
+
+    P->nprim_length = prim_size;
+
+    const int size = prim_size * sizeof(double);
+    const int size2 = shell12_size*sizeof(double); // for holding Xab, etc
+
+    // allocate one large space
+    double * mem = ALLOC(size * 11 + size2 * 3 ); 
+    P->x      = mem;
+    P->y      = mem +   prim_size;
+    P->z      = mem + 2*prim_size;
+    P->PA_x   = mem + 3*prim_size;
+    P->PA_y   = mem + 4*prim_size;
+    P->PA_z   = mem + 5*prim_size;
+    P->bAB_x  = mem + 6*prim_size;
+    P->bAB_y  = mem + 7*prim_size;
+    P->bAB_z  = mem + 8*prim_size;
+    P->alpha  = mem + 9*prim_size;
+    P->prefac = mem + 10*prim_size;
+
+    P->AB_x   = mem + 11*prim_size;
+    P->AB_y   = mem + 11*prim_size + shell12_size;
+    P->AB_z   = mem + 11*prim_size + 2*shell12_size;
+
+    int * intmem = ALLOC(prim_size * sizeof(int));
+    P->shellidx = intmem;
 }
 
 
@@ -122,10 +167,20 @@ void free_multishell_pair(struct multishell_pair P)
 }
 
 
+void free_multishell_pair_flat(struct multishell_pair_flat P)
+{
+   // Only need to free P.x since that points to the beginning of mem
+   FREE(P.x);
+
+   // similar with shellidx
+   FREE(P.shellidx);
+}
+
+
 
 void fill_multishell_pair(int na, struct gaussian_shell const * const restrict A,
-                             int nb, struct gaussian_shell const * const restrict B,
-                             struct multishell_pair * const restrict P)
+                          int nb, struct gaussian_shell const * const restrict B,
+                          struct multishell_pair * const restrict P)
 {
     ASSUME_ALIGN(A);
     ASSUME_ALIGN(B);
@@ -220,12 +275,118 @@ void fill_multishell_pair(int na, struct gaussian_shell const * const restrict A
     }
 }
 
+
+void fill_multishell_pair_flat(int na, struct gaussian_shell const * const restrict A,
+                               int nb, struct gaussian_shell const * const restrict B,
+                               struct multishell_pair_flat * const restrict P)
+{
+    ASSUME_ALIGN(A);
+    ASSUME_ALIGN(B);
+    ASSUME_ALIGN(P->x);
+    ASSUME_ALIGN(P->y);
+    ASSUME_ALIGN(P->z);
+    ASSUME_ALIGN(P->PA_x);
+    ASSUME_ALIGN(P->PA_y);
+    ASSUME_ALIGN(P->PA_z);
+    ASSUME_ALIGN(P->bAB_x);
+    ASSUME_ALIGN(P->bAB_y);
+    ASSUME_ALIGN(P->bAB_z);
+    ASSUME_ALIGN(P->alpha);
+    ASSUME_ALIGN(P->prefac);
+    ASSUME_ALIGN(P->shellidx);
+
+    int i, j, sa, sb, sasb, idx;
+
+    P->nshell1 = na;
+    P->nshell2 = nb;
+    P->nshell12 = na * nb;
+    P->nprim = 0;
+
+    sasb = 0;
+    idx = 0;
+    for(sa = 0; sa < na; ++sa)
+    {
+        P->am1 = A[sa].am;
+
+        for(sb = 0; sb < nb; ++sb)
+        {
+            // align to the next boundary
+            idx = SIMD_ROUND_DBL(idx);
+
+            P->am2 = B[sb].am;
+
+            // do Xab = (Xab_x **2 + Xab_y ** 2 + Xab_z **2)
+            const double Xab_x = A[sa].x - B[sb].x;
+            const double Xab_y = A[sa].y - B[sb].y;
+            const double Xab_z = A[sa].z - B[sb].z;
+            const double Xab = Xab_x*Xab_x + Xab_y*Xab_y + Xab_z*Xab_z;
+
+            ASSUME(idx%SIMD_ALIGN_DBL == 0);
+
+            ASSUME_ALIGN(A[sa].alpha);
+            ASSUME_ALIGN(A[sa].coef);
+            ASSUME_ALIGN(B[sb].alpha);
+            ASSUME_ALIGN(B[sb].coef);
+
+            for(i = 0; i < A[sa].nprim; ++i)
+            {
+                const double AxAa = A[sa].x * A[sa].alpha[i];
+                const double AyAa = A[sa].y * A[sa].alpha[i];
+                const double AzAa = A[sa].z * A[sa].alpha[i];
+
+                for(j = 0; j < B[sb].nprim; ++j)
+                {
+                    const double p_ab = A[sa].alpha[i] + B[sb].alpha[j];
+                    const double ABalpha = A[sa].alpha[i] * B[sb].alpha[j];
+
+                    P->prefac[idx] = A[sa].coef[i] * B[sb].coef[j]
+                                     * exp(-Xab * ABalpha / p_ab);
+
+                    P->x[idx] = (AxAa + B[sb].alpha[j]*B[sb].x)/p_ab;
+                    P->y[idx] = (AyAa + B[sb].alpha[j]*B[sb].y)/p_ab;
+                    P->z[idx] = (AzAa + B[sb].alpha[j]*B[sb].z)/p_ab;
+                    P->PA_x[idx] = P->x[idx] - A[sa].x;
+                    P->PA_y[idx] = P->y[idx] - A[sa].y;
+                    P->PA_z[idx] = P->z[idx] - A[sa].z;
+                    P->bAB_x[idx] = B[sb].alpha[j]*Xab_x;
+                    P->bAB_y[idx] = B[sb].alpha[j]*Xab_y;
+                    P->bAB_z[idx] = B[sb].alpha[j]*Xab_z;
+                    P->alpha[idx] = p_ab;
+                    P->shellidx[idx] = sasb;
+                    ++idx;
+                }
+
+            }
+
+            P->nprim += A[sa].nprim*B[sb].nprim;
+
+            P->AB_x[sasb] = Xab_x;
+            P->AB_y[sasb] = Xab_y;
+            P->AB_z[sasb] = Xab_z;
+
+            sasb++;
+        }
+    }
+}
+
+
 struct multishell_pair
 create_multishell_pair(int na, struct gaussian_shell const * const restrict A,
-                     int nb, struct gaussian_shell const * const restrict B)
+                       int nb, struct gaussian_shell const * const restrict B)
 {
     struct multishell_pair P;
     allocate_multishell_pair(na, A, nb, B, &P);
     fill_multishell_pair(na, A, nb, B, &P);
+    return P; 
+}
+
+
+struct multishell_pair_flat
+create_multishell_pair_flat(int na, struct gaussian_shell const * const restrict A,
+                            int nb, struct gaussian_shell const * const restrict B)
+{
+    struct multishell_pair_flat P;
+    allocate_multishell_pair_flat(na, A, nb, B, &P);
+    fill_multishell_pair_flat(na, A, nb, B, &P);
     return P; 
 }
--- eri/shell.h
+++ eri/shell.h
@@ -45,8 +45,37 @@ struct multishell_pair
     double * bAB_z;
     double * alpha;
     double * prefac;
-
 };
+
+
+struct multishell_pair_flat
+{
+    int am1, am2;          // angular momentum.
+    int nprim;             // Total number of primitives
+    int nprim_length;      // Actual length of alpha, etc, arrays (!= nprim due to alignment)
+    int nshell1, nshell2;  // number of shells
+    int nshell12;
+
+    // length nshell12
+    double * AB_x;
+    double * AB_y;
+    double * AB_z;
+
+    // these are all of length nprim
+    int * shellidx;
+    double * x;
+    double * y;
+    double * z;
+    double * PA_x;
+    double * PA_y;
+    double * PA_z;
+    double * bAB_x;
+    double * bAB_y;
+    double * bAB_z;
+    double * alpha;
+    double * prefac;
+};
+
 
 
 void allocate_gaussian_shell(int nprim, struct gaussian_shell * const restrict G);
@@ -54,20 +83,26 @@ void free_gaussian_shell(struct gaussian_shell G);
 void normalize_gaussian_shells(int n, struct gaussian_shell * const restrict G);
 
 
-void allocate_multishell_pair_from_shells(int na, struct gaussian_shell const * const restrict A,
-                                          int nb, struct gaussian_shell const * const restrict B,
-                                          struct multishell_pair * const restrict P);
-
 void free_multishell_pair(struct multishell_pair P);
+
+void free_multishell_pair_flat(struct multishell_pair_flat P);
 
 
 void fill_multishell_pair(int na, struct gaussian_shell const * const restrict A,
-                             int nb, struct gaussian_shell const * const restrict B,
-                             struct multishell_pair * const restrict P);
+                          int nb, struct gaussian_shell const * const restrict B,
+                          struct multishell_pair * const restrict P);
+
+void fill_multishell_pair_flat(int na, struct gaussian_shell const * const restrict A,
+                               int nb, struct gaussian_shell const * const restrict B,
+                               struct multishell_pair_flat * const restrict P);
 
 struct multishell_pair
 create_multishell_pair(int na, struct gaussian_shell const * const restrict A,
-                          int nb, struct gaussian_shell const * const restrict B);
+                       int nb, struct gaussian_shell const * const restrict B);
+
+struct multishell_pair_flat
+create_multishell_pair_flat(int na, struct gaussian_shell const * const restrict A,
+                            int nb, struct gaussian_shell const * const restrict B);
 
 #ifdef __cplusplus
 }
