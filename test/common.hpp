e8fc74b090e670f235d086932eb6f609be530030
Ben Pritchard <ben@bennyp.org>
2015-05-26 13:52:02 -0400

    Add openmp to the slow test code

--- test/common.hpp
+++ test/common.hpp
@@ -7,6 +7,10 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+    
+#ifdef _OPENMP
+#include <omp.h>
+#endif
 
 #include "eri/shell.h"
 
@@ -207,71 +211,82 @@ void ValeevIntegrals(const VecQuartet & gshells,
     const gaussian_shell * C = gshells[2].data();
     const gaussian_shell * D = gshells[3].data();
 
-    int nshell1 = gshells[0].size();
-    int nshell2 = gshells[1].size();
-    int nshell3 = gshells[2].size();
-    int nshell4 = gshells[3].size();
+    const int nshell1 = gshells[0].size();
+    const int nshell2 = gshells[1].size();
+    const int nshell3 = gshells[2].size();
+    const int nshell4 = gshells[3].size();
+    const int n234 = nshell2 * nshell3 * nshell4 * NCART(gshells[0][0].am) * NCART(gshells[1][0].am) * NCART(gshells[2][0].am) * NCART(gshells[3][0].am);
 
-    int idx = 0;
+    #ifdef _OPENMP
+        #pragma omp parallel for
+    #endif
     for(int i = 0; i < nshell1; i++)
-    for(int j = 0; j < nshell2; j++)
-    for(int k = 0; k < nshell3; k++)
-    for(int l = 0; l < nshell4; l++)
     {
-        double vA[3] = { A[i].x, A[i].y, A[i].z };
-        double vB[3] = { B[j].x, B[j].y, B[j].z };
-        double vC[3] = { C[k].x, C[k].y, C[k].z };
-        double vD[3] = { D[l].x, D[l].y, D[l].z };
+        const int idxstart = i * n234;
+        int idx = 0;
 
-        std::array<int, 3> g1{gshells[0][i].am, 0, 0};
-        do
+        for(int j = 0; j < nshell2; j++)
+        for(int k = 0; k < nshell3; k++)
+        for(int l = 0; l < nshell4; l++)
         {
-            std::array<int, 3> g2{gshells[1][j].am, 0, 0};
+            double vA[3] = { A[i].x, A[i].y, A[i].z };
+            double vB[3] = { B[j].x, B[j].y, B[j].z };
+            double vC[3] = { C[k].x, C[k].y, C[k].z };
+            double vD[3] = { D[l].x, D[l].y, D[l].z };
+
+            std::array<int, 3> g1{gshells[0][i].am, 0, 0};
             do
             {
-                std::array<int, 3> g3{gshells[2][k].am, 0, 0};
+                std::array<int, 3> g2{gshells[1][j].am, 0, 0};
                 do
                 {
-                    std::array<int, 3> g4{gshells[3][l].am, 0, 0};
+                    std::array<int, 3> g3{gshells[2][k].am, 0, 0};
                     do
                     {
-                        integrals[idx] = 0.0;
-
-                        for(int m = 0; m < A[i].nprim; m++)
-                        for(int n = 0; n < B[j].nprim; n++)
-                        for(int o = 0; o < C[k].nprim; o++)
-                        for(int p = 0; p < D[l].nprim; p++)
+                        std::array<int, 3> g4{gshells[3][l].am, 0, 0};
+                        do
                         {
-                            
-                            double val = Valeev_eri(g1[0], g1[1], g1[2], A[i].alpha[m], vA,
-                                                    g2[0], g2[1], g2[2], B[j].alpha[n], vB,
-                                                    g3[0], g3[1], g3[2], C[k].alpha[o], vC,
-                                                    g4[0], g4[1], g4[2], D[l].alpha[p], vD, 0);
-                            integrals[idx] += val * A[i].coef[m] * B[j].coef[n] * C[k].coef[o] * D[l].coef[p];
+                            double myint = 0.0;
 
-                            /*
-                            printf("IDX: %d\n", idx);
-                            printf("VAL: %8.3e\n", val);
-                            printf("%8.3e %8.3e %8.3e %8.3e\n", vA[0], vA[1], vA[2], A[i].alpha[m]);
-                            printf("%8.3e %8.3e %8.3e %8.3e\n", vB[0], vB[1], vB[2], B[j].alpha[n]);
-                            printf("%8.3e %8.3e %8.3e %8.3e\n", vC[0], vC[1], vC[2], C[k].alpha[o]);
-                            printf("%8.3e %8.3e %8.3e %8.3e\n", vD[0], vD[1], vD[2], D[l].alpha[p]);
-                            printf("%8.3e %8.3e %8.3e %8.3e\n", A[i].coef[m], B[j].coef[n], C[k].coef[o], D[l].coef[p]);
-                            std::cout << "i,j,k,l,idx = ";
-                            std::cout << g1[0] << "," << g1[1] << "," << g1[2] << "  ";
-                            std::cout << g2[0] << "," << g2[1] << "," << g2[2] << "  ";
-                            std::cout << g3[0] << "," << g3[1] << "," << g3[2] << "  ";
-                            std::cout << g4[0] << "," << g4[1] << "," << g4[2] << "  ";
-                            std::cout << idx << "  int = " << integrals[idx] << "\n";
-                            */
+                            for(int m = 0; m < A[i].nprim; m++)
+                            for(int n = 0; n < B[j].nprim; n++)
+                            for(int o = 0; o < C[k].nprim; o++)
+                            for(int p = 0; p < D[l].nprim; p++)
+                            {
+                                
+                                double val = Valeev_eri(g1[0], g1[1], g1[2], A[i].alpha[m], vA,
+                                                        g2[0], g2[1], g2[2], B[j].alpha[n], vB,
+                                                        g3[0], g3[1], g3[2], C[k].alpha[o], vC,
+                                                        g4[0], g4[1], g4[2], D[l].alpha[p], vD, 0);
+                                myint += val * A[i].coef[m] * B[j].coef[n] * C[k].coef[o] * D[l].coef[p];
 
-                        }
+/*
+                                printf("IDX: %d\n", idx);
+                                printf("VAL: %8.3e\n", val);
+                                printf("%8.3e %8.3e %8.3e %8.3e\n", vA[0], vA[1], vA[2], A[i].alpha[m]);
+                                printf("%8.3e %8.3e %8.3e %8.3e\n", vB[0], vB[1], vB[2], B[j].alpha[n]);
+                                printf("%8.3e %8.3e %8.3e %8.3e\n", vC[0], vC[1], vC[2], C[k].alpha[o]);
+                                printf("%8.3e %8.3e %8.3e %8.3e\n", vD[0], vD[1], vD[2], D[l].alpha[p]);
+                                printf("%8.3e %8.3e %8.3e %8.3e\n", A[i].coef[m], B[j].coef[n], C[k].coef[o], D[l].coef[p]);
+                                std::cout << "i,j,k,l,idx = ";
+                                std::cout << g1[0] << "," << g1[1] << "," << g1[2] << "  ";
+                                std::cout << g2[0] << "," << g2[1] << "," << g2[2] << "  ";
+                                std::cout << g3[0] << "," << g3[1] << "," << g3[2] << "  ";
+                                std::cout << g4[0] << "," << g4[1] << "," << g4[2] << "  ";
+                                std::cout << idx << "  int = " << integrals[idx] << "\n";
+*/
 
-                        idx++;
-                    } while(IterateGaussian(g4));
-                } while(IterateGaussian(g3));
-            } while(IterateGaussian(g2));
-        } while(IterateGaussian(g1));
+
+                            }
+
+                            integrals[idxstart + idx] = myint;
+                            idx++;
+
+                        } while(IterateGaussian(g4));
+                    } while(IterateGaussian(g3));
+                } while(IterateGaussian(g2));
+            } while(IterateGaussian(g1));
+        }
 
 
     }
