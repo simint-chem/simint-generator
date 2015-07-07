9f35d4a4ea30671a896b40034f249e56e517e763
Ben Pritchard <ben@bennyp.org>
2015-07-07 13:12:33 -0400

    Fix KNC sqrt macro

--- vectorization/intrinsics_kncni.h
+++ vectorization/intrinsics_kncni.h
@@ -13,6 +13,6 @@ union double8
 
 // These aren't defined in KNCI, so make some macros
 #define MM512_SET1_PD(val) (((union double8){ .d_256 = { _mm256_set1_pd((val)), _mm256_set1_pd((val)) } }).d_512)
-#define MM512_SQRT_PD(val) (((union double8){ .d_256 = { _mm256_sqrt_pd((val)), _mm256_sqrt_pd((val)) } }).d_512)
+#define MM512_SQRT_PD(val) (((union double8){ .d_256 = { _mm256_sqrt_pd( (((union double8)((val))).d_256[0]) ), _mm256_sqrt_pd( (((union double8)((val))).d_256[0]) )  } }).d_512)
 
 #endif
