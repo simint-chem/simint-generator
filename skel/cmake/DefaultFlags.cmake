2441368f0eed185f56520a46df1824fab1ee190d
Ben Pritchard <ben@bennyp.org>
2017-03-18 12:28:21 -0400

    Cmake fixes, particularly with shared libs

--- skel/cmake/DefaultFlags.cmake
+++ skel/cmake/DefaultFlags.cmake
@@ -12,13 +12,17 @@ if("${CMAKE_C_COMPILER_ID}" MATCHES "Intel")
   list(APPEND SIMINT_C_FLAGS "-qopenmp")
   list(APPEND SIMINT_C_FLAGS "-qopt-report=5;-w3")
   list(APPEND SIMINT_C_FLAGS "-wd10397;-wd2415;-wd981;-wd869;-wd177")  # Remove wd177 (unused variable) at some point
-  list(APPEND SIMINT_C_LINK_FLAGS "-qopenmp")
+  list(APPEND SIMINT_LINK_FLAGS "-qopenmp")
 
   list(APPEND SIMINT_TESTS_CXX_FLAGS "-std=c++11;-restrict")
   list(APPEND SIMINT_TESTS_CXX_FLAGS "-qopenmp")
   list(APPEND SIMINT_TESTS_CXX_FLAGS "-qopt-report=5;-w3")
   list(APPEND SIMINT_TESTS_CXX_FLAGS "-wd10397;-wd981;-wd383")
-  list(APPEND SIMINT_TESTS_CXX_LINK_FLAGS "-qopenmp")
+  list(APPEND SIMINT_TESTS_LINK_FLAGS "-qopenmp")
+
+  if(SIMINT_STANDALONE)
+    list(APPEND SIMINT_LINK_FLAGS "-static-libgcc;-static-intel;-wd10237")
+  endif()
 
 
 elseif("${CMAKE_C_COMPILER_ID}" MATCHES "GNU" OR
@@ -38,9 +42,14 @@ elseif("${CMAKE_C_COMPILER_ID}" MATCHES "GNU" OR
   if("${CMAKE_C_COMPILER_ID}" MATCHES "GNU")
     # OpenMP is enabled by default with clang
     list(APPEND SIMINT_C_FLAGS "-fopenmp")
-    list(APPEND SIMINT_C_LINK_FLAGS "-fopenmp")
+    list(APPEND SIMINT_LINK_FLAGS "-fopenmp")
     list(APPEND SIMINT_TESTS_CXX_FLAGS "-fopenmp")
-    list(APPEND SIMINT_TESTS_CXX_LINK_FLAGS "-fopenmp")
+    list(APPEND SIMINT_TESTS_LINK_FLAGS "-fopenmp")
+
+    if(SIMINT_STANDALONE)
+      list(APPEND SIMINT_LINK_FLAGS "-static-libgcc")
+    endif()
+
   endif()
 
 else()
@@ -49,10 +58,32 @@ else()
 
 endif()
 
+# Check for unspecified or invalid vectorization type
+set(SIMINT_VALID_VECTOR
+     scalar
+     scalar-sse
+     scalar-avx
+     scalar-avxfma
+     scalar-micavx512
+     sse
+     avx
+     avxfma
+     micavx512
+)
+
+if("${SIMINT_VECTOR}" STREQUAL "")
+  message(FATAL_ERROR "Vectorization not specified. Specify SIMINT_VECTOR")
+endif()
 
 # SIMINT_VECTOR required to be set (it may be empty here, hence the "")
 string(TOLOWER "${SIMINT_VECTOR}" SIMINT_VECTOR_LOWER)
 string(TOUPPER "${SIMINT_VECTOR}" SIMINT_VECTOR_UPPER)
+
+list(FIND SIMINT_VALID_VECTOR "${SIMINT_VECTOR_LOWER}" SIMINT_VECTOR_IDX)
+if(${SIMINT_VECTOR_IDX} EQUAL -1)
+  message(FATAL_ERROR "Invalid vectorization type ${SIMINT_VECTOR} specified")
+endif()
+
 
 # Handle scalar
 if(SIMINT_VECTOR_LOWER STREQUAL "scalar")
